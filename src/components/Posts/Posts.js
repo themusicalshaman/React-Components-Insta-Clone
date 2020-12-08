2fa30998820c08113f6c8bec5c1f49b32e553a24
themusicalshaman <ryanshipley27@gmail.com>
2020-12-08 16:18:36 -0700

    struggling with a render issue

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -10,6 +10,11 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map(postObject => {
+        return (
+          <Post key= {postObject.id} likePost= {likePost} post= {postObject} numberOfLikes= {postObject.likes} comments = {postObject.comments} />
+        )
+      })}
     </div>
   );
 };
